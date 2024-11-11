a046fe1a9595ad1363256f7fcd9ef8cc05e0a6c6
thomas <bezille.thomasb@gmail.com>
2024-11-11 15:35:39 +0100

    📦 NEW: Creating the reference file and retrieving the path

---
+++ file_sorter.py
@@ -0,0 +1,12 @@
+""" Méthode de tri :
+    -    mp3, wav, flac : Musique
+    -    avi, mp4, gif : Videos
+    -    bmp, png, jpg : Images
+    -    txt, pptx, csv, xls, odp, pages : Documents
+    -    autres : Divers
+"""
+
+from pathlib import Path
+
+folder = Path("/c/xampp/htdocs/DEV/PYTHON/formation_python_trieur_de_fichiers")
+
--- main.py
+++
@@ -1,8 +0,0 @@
-"""
-Trier les fichiers contenus dans le dossier data selon les associations suivantes :
-mp3, wav, flac : Musique
-avi, mp4, gif : Videos
-bmp, png, jpg : Images
-txt, pptx, csv, xls, odp, pages : Documents
-autres : Divers
-"""
